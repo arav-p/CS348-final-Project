e1294105e74485c4e1785a66edc351229b395cd7
Ubuntu <ubuntu@ip-172-31-17-221.ec2.internal>
2025-05-06 03:07:51 +0000

    index enabled

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from db_setup import db
 class Workout(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.String(20), nullable=False)
+    date = db.Column(db.String(20), nullable=False, index=True)
     exercises = db.relationship('Exercise', backref='workout', cascade="all, delete")
 
 class Exercise(db.Model):
